513812b3a2fc1849c6fab72dd6f6fe44ce0c7d70
Chand Singh <twiliosms97@gmail.com>
2024-01-14 08:31:12 +0530

    My Blog Project Ready

---
+++ src/main/java/com/myblog/myblog12/Myblog12Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog12Application.class, args);
+	}
+
+}
